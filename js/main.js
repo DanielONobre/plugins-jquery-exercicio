803606757f9b901c8570813bbca62bb3457e6f26
Carlos Daniel Farias Nobre <106852463+DanielONobre@users.noreply.github.com>
2025-03-07 18:26:47 -0300

    Exercício corrigido
    Correções realizadas

--- js/main.js
+++ js/main.js
@@ -1,42 +1,47 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$(document).ready(function(){
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        arrows: false
+    });
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+$('#telefone').mask('(00) 00000-0000')
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+$('#cpf').mask('000.000.000-00')
+
+$('#cep').mask('00000-000')
 
 $('form').validate({
     rules: {
         nome: {
-            requerido: true
+            required: true
         },
         email: {
-            requerido: true,
+            required: true,
             email: true
         },
         telefone: {
-            requerido: true
+            required: true
         },
         endereco: {
-            requerido: true
+            required: true
         },
         cep: {
-            requerido: true
+            required: true
         },
         cpf: {
-            requerido: true
+            required: true
         },
     },
     submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
+        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!")
+        console.log(form)
     },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
+    invalidHandler: function (evento, validator) {
+        let camposIncorretos = validator.numberOfInvalids();
+        console.log(camposIncorretos)
+        if (camposIncorretos){
+            alert("Por favor, preencha os campos para prosseguir com a compra!");
+        }
+   }
+})
 })
